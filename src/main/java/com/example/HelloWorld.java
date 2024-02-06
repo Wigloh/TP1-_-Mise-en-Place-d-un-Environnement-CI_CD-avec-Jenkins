813b9177684666ec8546bc2dc14e7b1a2e7f8631
LouisFv <louis.fievet@ecoles-epsi.net>
2024-02-06 13:46:34 +0100

    II

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World i test my webhook !");
+        System.out.println("Hello, World i test my webhook !!");
     }
 }
